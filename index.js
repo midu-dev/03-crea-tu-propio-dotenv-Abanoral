7a7d407f1ad05e80481fd0e6ab5b9249235a17bb
Alejandro <66677531+Abanoral@users.noreply.github.com>
2023-10-28 13:35:29 +0200

    Tercer ejercicio

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-// reference: https://www.npmjs.com/package/dotenv
+const fs = require('node:fs')
 
 function config (options = {}) {
-  // your code here
+  const filePath = options.path || '.env'
+  let dotenvFile
+  try {
+    dotenvFile = fs.readFileSync(filePath, 'utf8')
+  } catch (err) {
+
+  }
+  if (!dotenvFile) return
+  const envVariables = dotenvFile.split('\n')
+
+  for (const linetext of envVariables) {
+    const [key, value] = linetext.split('=')
+    process.env[key] = value.replaceAll('"', '')
+  }
 }
 
 module.exports = { config }
